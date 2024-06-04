b1f75fdf8c511008c5bb4e914499a9caf3c674ae
MFFTD <user1@email.com>
2024-06-04 17:07:02 +0300

    add summary message

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Defence-level-checker
-##Check players defence level
+Defence-level-checker is used alongside with RuneLite plugin "Clan List Exporter", which exports player names of a friends chat/clan chat -channel into a csv file.
+
+## Requirements
+Python 3.6 or higher
+
+## Usage
+```sh
+1. Export the friends chat/clan chat -channel names to a csv file using RuneLite plugin Clan List Exporter. The plugin is found from plugin hub
+2. Navigate to your .runelite folder and get the csv file(s) from clanlistexports folder and move them to this projects csv_files folder
+3. run the python script
+  - for example; navigate to the project folder in command line and run this command python3 scrape.py  
+```
--- scrape.py
+++ scrape.py
@@ -6,6 +6,7 @@ import requests
 class DefenceLevelChecker:
     def __init__(self):
         self.count: int = 0
+        self.users_with_high_defence_count: int = 0
 
     def get_current_folder_path(self) -> str:
         return os.getcwd()
@@ -30,6 +31,9 @@ class DefenceLevelChecker:
                 usernames.pop()
         return usernames
 
+    def starting_to_scrape_message(self, length: int) -> None:
+        print(f"Starting to check Defence levels of [{length}] users...")
+
     def scrape(self, usernames: list[str]) -> list[str]:
         for username in usernames:
             request_response = requests.get(f'https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player={username}')
@@ -37,14 +41,31 @@ class DefenceLevelChecker:
             self.check_defence_levels(username, rows)
 
     def check_defence_levels(self, username: str, rows: list[str]) -> None:
+        self.count += 1
         if len(rows) >= 3: # 3rd row contains the data for defence
             defence_row = rows[2] # index for the defence data row
             defence_values = defence_row.split(',') # the row contains rank, level, total xp
             if len(defence_values) >= 2:
                 defence_level = int(defence_values[1]) # get the element that contains defence level
                 if defence_level > 25:
-                    self.count += 1
-                    print(f'{self.count}. Defence level for "{username}": {defence_level}.')
+                    self.users_with_high_defence_count += 1
+                    print(f'{self.users_with_high_defence_count}. Defence level for "{username}": {defence_level}.')
+
+    def scraping_completed_message(self, csv_length: int) -> None:
+        print(f"Summary:")
+        print(f"* csv file consisted [{csv_length}] names")
+        print(f"* found [{self.count}]/[{csv_length}] names from hiscores")
+        if self.count < csv_length:
+            print(f"--------------------------------------------------------------------------")
+            print(f"not all players were found from hiscores")
+            print(f"this could happen for various reasons etc:")
+            print(f"* name changes")
+            print(f"* f2p players")
+            print(f"you can get a new csv file and re-run the script to check for name changes")
+            print(f"--------------------------------------------------------------------------")
+        print(f"* [{self.users_with_high_defence_count}] users higher than 25 defence")
+
+
 
     def main(self) -> None:
         current_working_directory_path = self.get_current_folder_path()
@@ -53,7 +74,9 @@ class DefenceLevelChecker:
         sorted_files = self.sort_files_by_creation_time(csv_files_path)
         latest_file = self.get_latest_file(sorted_files)
         usernames = self.read_csv(latest_file)
+        self.starting_to_scrape_message(len(usernames))
         self.scrape(usernames)
+        self.scraping_completed_message(len(usernames))
 
 if __name__ == "__main__":
     checker = DefenceLevelChecker()
